12be2dac84d114d26083f66f09487906d21bb620
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-16 20:42:58 +0300

    Update Task_2.java

--- src/homework3/task_2/Task_2.java
+++ src/homework3/task_2/Task_2.java
@@ -2,8 +2,10 @@ package homework3.task_2;
 
 public class Task_2 {
     public static void main(String[] args) {
+        // num - это один, а у тебя numbers
         int num [] = {5, 8, 12, 18, 32, 84, 63, 17, 37};
         double sum = 0;
+        // x -> number
         for (int x: num) {
             sum += x;
         }
