871481b42d1743b9465d4ada9b3252e7b15229e0
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-16 20:47:09 +0300

    Update Task_5.java

--- src/homework3/task_5/Task_5.java
+++ src/homework3/task_5/Task_5.java
@@ -5,11 +5,12 @@ import java.util.Arrays;
 public class Task_5 {
     public static void main(String[] args) {
         int[] array = {0, 1, 2, 3, 4};
+        // эту задачу можно решить дойдя только до середины массива, пока не оптимально
         for (int i = 0; i < array.length; i++) {
             array[i] = array.length - 1 - i;
         }
         System.out.println(Arrays.toString(array));
-
-
+// пустая строка не нужа
+// пустая строка не нужа
     }
 }
