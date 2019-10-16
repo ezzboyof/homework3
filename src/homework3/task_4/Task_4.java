210a3c5340823e29ba3ab2139989d0884845e613
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-16 20:45:46 +0300

    Update Task_4.java

--- src/homework3/task_4/Task_4.java
+++ src/homework3/task_4/Task_4.java
@@ -1,5 +1,6 @@
 package homework3.task_4;
 
+// не очень красиво, но выводит
 public class Task_4 {
     public static void main(String[] args) {
         int[][] array = new int[10][10];
@@ -8,12 +9,12 @@ public class Task_4 {
             for (int j = 0; j < array[i].length; j++) {
                 array[i][j] = (1 + i) * (1 + j);
                 System.out.print(array[i][j] + " ");
-
+// пустая строка не нужа
             }
-
+// пустая строка не нужа
             System.out.println();
         }
-
-
+// пустая строка не нужа
+// пустая строка не нужа
     }
 }
